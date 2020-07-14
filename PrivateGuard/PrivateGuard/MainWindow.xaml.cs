e865759c0ba13192740aa3feee0d7d9e8cc4c3ad
Jacob <65638832+jluvisi2021@users.noreply.github.com>
2020-07-13 17:02:56 -0700

    Updated Layouyt

--- PrivateGuard/PrivateGuard/MainWindow.xaml.cs
+++ PrivateGuard/PrivateGuard/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ using System.Windows.Shapes;
 
 namespace PrivateGuard
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static float VersionID = 0.1F;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            SetupPrimaryScreen();
+        }
+
+        /// <summary>
+        /// Organizes the primary screen through code rather than XAML.
+        /// </summary>
+        void SetupPrimaryScreen()
+        {
+            MainWindow mw = this;
+            mw.Title = "Private Guard | Version: " + VersionID;
+            mw.VersionLabel.Content = "Version: " + VersionID;
+            
         }
     }
 }
