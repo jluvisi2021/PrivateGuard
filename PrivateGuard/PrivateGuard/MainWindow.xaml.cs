e3550d3e31781a397fd79a703fa80091c2e44769
Jacob <65638832+jluvisi2021@users.noreply.github.com>
2020-07-14 15:42:03 -0700

    MainWindow visual update.
    Added Private Guard Shield and removed WindowBar for new custom tool bar.
    Added username, filekey and selected file fields.
    Added Show/Hide button for filekey.

--- PrivateGuard/PrivateGuard/MainWindow.xaml.cs
+++ PrivateGuard/PrivateGuard/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +22,7 @@ namespace PrivateGuard
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool ShowFileKeyField = false;
         public static float VersionID = 0.1F;
 
 
@@ -36,9 +38,41 @@ namespace PrivateGuard
         void SetupPrimaryScreen()
         {
             MainWindow mw = this;
-            mw.Title = "Private Guard | Version: " + VersionID;
+            //mw.Title = "Private Guard | Version: " + VersionID;
             mw.VersionLabel.Content = "Version: " + VersionID;
+            //mw.WindowStyle = new WindowStyle {  }; // Make blank top bar.
+        }
+
+        private void ExitProgramLabel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(MessageBox.Show("Exit PrivateGuard?", "Exit", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
+            {
+                Close();
+            }
             
+        }
+
+        private void ExitProgramLabel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Label ExitLabel = sender as Label;
+            ExitLabel.Foreground = new SolidColorBrush(Color.FromRgb(184, 186, 189));
+        }
+
+        private void ExitProgramLabel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Label ExitLabel = sender as Label;
+            ExitLabel.Foreground = new SolidColorBrush(Color.FromRgb(31, 31, 33));
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove(); // Allows the dragging of the application.
+        }
+
+        private void ToggleViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowFileKeyField = !ShowFileKeyField;
+            ToggleViewButton.Content = ShowFileKeyField ? ToggleViewButton.Content = "Hide" : ToggleViewButton.Content = "Show";
         }
     }
 }
