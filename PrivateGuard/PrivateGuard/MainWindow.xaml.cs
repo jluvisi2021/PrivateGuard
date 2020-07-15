0bf8304f58524e2bd557dc4a0eae3495e429beb1
Jacob <65638832+jluvisi2021@users.noreply.github.com>
2020-07-14 18:15:20 -0700

    Added Encryption/Decryption/Button Options
    - Added Encryption/Decryption using a WIP FileKey.
    Added FIle selection and file open menus.

--- PrivateGuard/PrivateGuard/MainWindow.xaml.cs
+++ PrivateGuard/PrivateGuard/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PrivateGuard.PG_Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,14 +26,38 @@ namespace PrivateGuard
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string username = "jluvisi";
+
         bool ShowFileKeyField = false;
         public static float VersionID = 0.1F;
 
+        public void TestRWBinaryFile()
+        {
+            String key = "8UHjPgXZzXCGkhxV2QCnooyJexUzvJrO";
 
+
+            FileStream fs = new FileStream("C:\\Users\\jluvi\\Desktop\\test.pgm", FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            
+            bw.Write(AES.Encrypt("Hello World! (2)", key));
+         
+            bw.Close();
+            fs.Close();
+            FileStream fs2 = new FileStream("C:\\Users\\jluvi\\Desktop\\test.pgm", FileMode.Open);
+            BinaryReader bw2 = new BinaryReader(fs2);
+
+            string data = AES.Decrypt(bw2.ReadString(), key);
+            //string data = bw2.ReadString();
+            SelectedFileField.Text = data;
+
+        }
         public MainWindow()
         {
+
+            
             InitializeComponent();
             SetupPrimaryScreen();
+            TestRWBinaryFile();
         }
 
         /// <summary>
@@ -41,6 +69,9 @@ namespace PrivateGuard
             //mw.Title = "Private Guard | Version: " + VersionID;
             mw.VersionLabel.Content = "Version: " + VersionID;
             //mw.WindowStyle = new WindowStyle {  }; // Make blank top bar.
+            OpenFileButton.BorderThickness = new Thickness(2.5);
+            NewFileButton.BorderThickness = new Thickness(2.5);
+            DocumentationButton.BorderThickness = new Thickness(2.5);
         }
 
         private void ExitProgramLabel_MouseDown(object sender, MouseButtonEventArgs e)
@@ -73,6 +104,134 @@ namespace PrivateGuard
         {
             ShowFileKeyField = !ShowFileKeyField;
             ToggleViewButton.Content = ShowFileKeyField ? ToggleViewButton.Content = "Hide" : ToggleViewButton.Content = "Show";
+        }
+
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open Window Selector and find .txt file (or binary file) to open.
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".pgm";
+            dlg.Filter = "PGM Files (*.pgm)|*.pgm";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string fileName = dlg.FileName;
+                SelectedFileField.Text = fileName;
+            }
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(UsernameField.Text != username)
+            {
+                DisplayErrorMessage(ERROR_TYPES.WRONG_CREDENTIALS);
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(FileKeyField.Text))
+            {
+                DisplayErrorMessage(ERROR_TYPES.NO_FILE_KEY);
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(SelectedFileField.Text))
+            {
+                DisplayErrorMessage(ERROR_TYPES.NO_FILE_SELECTED);
+                return;
+            }
+            FileStream fs2 = new FileStream(SelectedFileField.Text, FileMode.Open);
+            BinaryReader bw2 = new BinaryReader(fs2);
+            string modifier_value;
+            try
+            {
+                modifier_value = AES.Decrypt(bw2.ReadString(), FileKeyField.Text);
+
+            }
+            catch (EncoderFallbackException)
+            {
+                DisplayErrorMessage(ERROR_TYPES.WRONG_CREDENTIALS);
+                return;
+            }
+            //string data = bw2.ReadString();
+            if(modifier_value != "OKAY_TO_ACCESS_MODIFIER_VALUE")
+            {
+                DisplayErrorMessage(ERROR_TYPES.WRONG_CREDENTIALS);
+                return;
+            }
+            fs2.Close();
+            bw2.Close();
+            MessageBox.Show("ALL GOOD!");
+            // All is good attempt to open file.
+
+        }
+
+        /// <summary>
+        /// Display a message to the user.
+        /// </summary>
+        /// <param name="e"></param>
+        private static void DisplayErrorMessage(ERROR_TYPES e)
+        {
+            switch(e)
+            {
+                case ERROR_TYPES.WRONG_CREDENTIALS:
+                    MessageBox.Show("Incorrect Credentials.", "Error logging in.", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case ERROR_TYPES.NO_FILE_SELECTED:
+                    MessageBox.Show("No file selected. Please select a file using the \"Select\" button.", "Error logging in.", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case ERROR_TYPES.NO_FILE_KEY:
+                    MessageBox.Show("No file key found. Please fill out the file key text box with the respective file key for the selected file.", "Error logging in.", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                default:
+                    Console.WriteLine("Could not find error: " + e.ToString());
+                    break;
+            }
+        }
+
+        enum ERROR_TYPES
+        {
+            WRONG_CREDENTIALS, NO_FILE_SELECTED, NO_FILE_KEY
+        }
+
+        private void DocumentationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            String FileKey = "8UHjPgXZzXCGkhxV2QCnooyJexUzvJrO";
+
+            Stream myStream;
+            SaveFileDialog sfd = new SaveFileDialog();
+            
+
+            sfd.Filter = "PGM Files (*.pgm)|*.pgm";
+            sfd.FilterIndex = 1;
+            sfd.FileName = "MyManager";
+            sfd.RestoreDirectory = true;
+            Nullable<bool> result = sfd.ShowDialog();
+
+            if (result==true)
+            {
+                if ((myStream = sfd.OpenFile()) != null)
+                {
+                    MessageBox.Show($"Saved file at: {sfd.FileName}! \nTo access and/or edit the file input the file key and then use the \"Open File\" button.","Saved File", MessageBoxButton.OK, MessageBoxImage.Information);
+                    myStream.Close();
+                    Dispatcher.Invoke(() =>
+                    {
+                        FileStream fs = new FileStream(sfd.FileName, FileMode.Open);
+                        BinaryWriter bw = new BinaryWriter(fs);
+                        bw.Write(AES.Encrypt("OKAY_TO_ACCESS_MODIFIER_VALUE", FileKey));
+
+                        bw.Close();
+                        fs.Close();
+                    });
+                   
+                    // Code to write the stream goes here.
+                    
+                    
+                }
+            }
         }
     }
 }
